6efd6655c671dfc8cc78a415fcd82686405d161f
jung0930 <wjddlqkqh57@naver.com>
2021-05-25 14:17:04 +0900

    refactor: Numbers도메인 생성자변경, addNumber 메소드 추가

--- src/main/java/baseBall/domain/Numbers.java
+++ src/main/java/baseBall/domain/Numbers.java
@@ -1,20 +1,22 @@
 package baseBall.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Numbers {
 
     public static final String SIZE_ERROR_MESSAGE = "3개의 숫자를 입력해야합니다.";
     public static final String OTHER_ERROR_MESSAGE = "모두 다른 숫자를 입력해야합니다.";
-    private final List<Number> numbers;
+    public static final int SUCCESS_SIZE = 3;
+    private final List<Number> numbers = new ArrayList<>();
 
-    public Numbers(List<Number> numbers) {
-        this.numbers = numbers;
+    public void addNumber(Number number) {
+        numbers.add(number);
     }
 
     // 입력한 숫자가 3개인지 확인
     public boolean sizeCheck() {
-        if(numbers.size() != 3) {
+        if(numbers.size() != SUCCESS_SIZE) {
             throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
         }
 
@@ -24,14 +26,35 @@ public class Numbers {
     // 입력한 숫자가 모두 다른 값인지 확인
     public boolean otherCheck() {
 
-        for(Number number : numbers) {
+        // OTHER_ERROR_MESSAGE
+
+        for(int i = 0; i < numbers.size(); i++) {
+
         }
 
-        // OTHER_ERROR_MESSAGE
+        /*
+        List<Integer> list = new ArrayList<>();
+        int num = numbers.get(0).getNumber();
+        list.add(num);
+        int index = 0;
+
+        for(Number number : numbers) {
+            if(index == 0) {
+                continue;
+            }
+
+            if(num == number.getNumber()) {
+                throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
+            }
+        }
+
+        */
+
+        /*
         if(numbers.size() != 3) {
             throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
         }
-
+        */
         return true;
     }
 
--- src/test/java/baseBall/domain/NumbersTest.java
+++ src/test/java/baseBall/domain/NumbersTest.java
@@ -1,9 +1,11 @@
 package baseBall.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import stringCalculator.StringAddCalculator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,15 +18,24 @@ class NumbersTest {
 
     private Numbers numbers;
 
+    @BeforeEach
+    void setup() {
+        numbers = new Numbers();
+    }
+
     @DisplayName("입력한 숫자가 3개일 경우 TRUE RETURN")
     @Test
     public void sizeCheck_사이즈정상처리() {
         //given
+        numbers.addNumber(new Number(1));
+        numbers.addNumber(new Number(2));
+        numbers.addNumber(new Number(3));
+
         List<Number> list = new ArrayList<>();
         list.add(new Number(1));
         list.add(new Number(3));
         list.add(new Number(5));
-        numbers = new Numbers(list);
+        // numbers = new Numbers(list);
 
         //when
 
@@ -36,12 +47,10 @@ class NumbersTest {
     @Test
     public void sizeCheck_범위예외발생() {
         //given
-        List<Number> list = new ArrayList<>();
-        list.add(new Number(1));
-        list.add(new Number(3));
-        list.add(new Number(5));
-        list.add(new Number(7));
-        numbers = new Numbers(list);
+        numbers.addNumber(new Number(1));
+        numbers.addNumber(new Number(2));
+        numbers.addNumber(new Number(3));
+        numbers.addNumber(new Number(4));
 
         //when
 
@@ -55,11 +64,10 @@ class NumbersTest {
     @Test
     public void otherCheck_서로다른수정상처리() {
         //given
-        List<Number> list = new ArrayList<>();
-        list.add(new Number(1));
-        list.add(new Number(3));
-        list.add(new Number(5));
-        numbers = new Numbers(list);
+        numbers.addNumber(new Number(1));
+        numbers.addNumber(new Number(2));
+        numbers.addNumber(new Number(3));
+        // numbers = new Numbers(list);
 
         //when
 
